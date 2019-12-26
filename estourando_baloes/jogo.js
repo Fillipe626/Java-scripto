a6758d4505b22dacc091fe2a3e77cc3fb27b5d22
Fillipe Palhares <lpjp91@gmail.com>
2019-12-26 17:48:52 -0400

    Correção do bug que permitia que o balao fosse estourado mais de uma vez

--- estourando_baloes/jogo.js
+++ estourando_baloes/jogo.js
@@ -78,6 +78,7 @@ function cria_baloes(qtde_baloes) {
 function estourar(e){
 
 	var id_balao = e.id;
+	document.getElementById(id_balao).setAttribute("onclick","") //Limpa o evento e evita que o mesmo balao seja estourado varias vezes
 	document.getElementById(id_balao).src = 'imagens/balao_azul_pequeno_estourado.png';
 
 	pontuacao(-1);
@@ -109,4 +110,15 @@ function situacao_jogo(baloes_inteiros){
 	function parar_jogo(){
 		clearTimeout(timerId);
 	}
+}
+
+function remove_eventos_baloes() {
+    var i = 1; //contado para recuperar balões por id
+
+    //percorre o lementos de acordo com o id e só irá sair do laço quando não houver correspondência com elemento
+    while(document.getElementById('b'+i)) {
+        //retira o evento onclick do elemnto
+        document.getElementById('b'+i).onclick = '';
+        i++; //faz a iteração da variávei i
+    }
 }
